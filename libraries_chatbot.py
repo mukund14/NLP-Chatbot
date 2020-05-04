73039a01b6bde01c70f2c37a4a848f5764f2be5b
Mukundan Sankar <mukund14@users.noreply.github.com>
2020-05-03 21:58:53 -0700

    Add files via upload

---
+++ libraries_chatbot.py
@@ -0,0 +1,9 @@
+lst=['nltk','nltk.chat.util','re','random','webbrowser', 'get_all_tickers','yfinance' ,'yahoo_fin',
+'flask','numpy','wordcloud','matplotlib','PIL','wordcloud','pandas','plotly.express','sent_analy','nltk.corpus',
+'nltk.sentiment','nltk.sentiment.util' ,'nltk.tokenize','sklearn.feature_extraction.text' ,'sklearn' ,'os'
+'newsapi','pandas.io.json']
+
+def func(lst):
+	for i in lst:
+		return pip install i
+func(lst)
